f7220438fb11831b37540d7f7991eb5d7556f64c
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-05-04 01:35:19 +0600

    install cors to share data on client site

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const chef = require('./Data/chef.json');
 const recipe = require('./Data/recipe.json')
 const app = express();
-const cors = require('cors');
+const cors = require('');
 const port = process.env.PORT || 5000;
 app.use(cors());
 
